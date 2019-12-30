d58175d17551b0c0ca42882800168544e4a5476a
fuadmiazi <fuad.hasan.1997@gmail.com>
2019-12-30 20:44:37 +0600

    Instructors

---
+++ instructors.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="http://localhost/instructors.css">
+</head>
+<body>
+
+    <div class="main-container">
+        <div class="flex-container">
+            <div class="item menu1"> <a id="linked" href="http://localhost/homesi.php"> Home</a></div>
+            <div class="item menu2"> <a id="linked" href="http://localhost/courses.php">Courses</a> </div>
+            <div class="item menu3"><a id="linked" href="http://localhost/instructors.php">Instructors</a> </div>
+            <div class="item menu4"><a id="linked" href="http://localhost/about.php">About</a></div>
+            <div class="item menu5"><a id="linked" href="http://localhost/cart.php">Cart</a> </div>
+            <div class="item menu5"><a id="linked" href="http://localhost/logout.php">Log Out</a> </div>
+        </div> 
+
+    <div class="headline"><u>Instructors: </u> </div>
+
+    <div class="group-members">
+
+        <div class="member1">
+            <div class="image">
+                <img id="image" src="http://localhost/sir.jpg">
+            </div>
+            <div class="information">
+                <div class="name">Razib Hayat Khan</div>
+                <div class="designation">Faculty</div>
+                <div class="institite">North South University</div>
+            </div>
+        </div>
+
+        <div class="member1">
+            <div class="image">
+                <img id="image" src="http://localhost/nafi.jpg">
+            </div>
+            <div class="information">
+                <div class="name">Md Nafi</div>
+                <div class="designation">Lab Instructor</div>
+                <div class="institite">North South University</div>
+            </div>
+        </div>
+
+
+    </div>
+
+    </div>
+</body>
+</html>
